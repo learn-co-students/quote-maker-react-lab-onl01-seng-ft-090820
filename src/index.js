237c881acd079676b62ef50c23439f4dcc0eb2e0
John Zonneveld <jjczon@gmail.com>
2021-01-30 20:09:22 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux'
 import rootReducer from './reducers/index'
 
-const store = createStore(
-  rootReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && 
-  window.__REDUX_DEVTOOLS_EXTENSION__())
+
+let store = createStore(rootReducer)
+
+// const store = createStore(
+//   rootReducer, 
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && 
+//   window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
